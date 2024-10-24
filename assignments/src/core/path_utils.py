71b5e860fb8b9218831b7924a26bae63dd49b672
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2024-10-24 19:44:14 -0400

    updated code for step 10

--- assignments/src/core/path_utils.py
+++ assignments/src/core/path_utils.py
@@ -2,16 +2,17 @@ import os
 
 def get_data_path(relative_path: str) -> str:
     """
-    Get the absolute path to a file in the dataset directory.
+    Get the absolute path to a file in the `dataset` directory.
 
     Args:
-        relative_path: The path to the file relative to the dataset directory including the file name.
+        relative_path: The path to the file relative to the `dataset` directory including the file name.
 
     Returns:
-        The full path to the file in the dataset directory.
+        The full path to the file in the `dataset` directory.
 
     Examples:
         >>> get_data_path("assignment1/boston_listings.csv")
         'C:/Users/username/assignments/dataset/assignment1/boston_listings.csv'
     """
-    return os.path.join(os.path.dirname(__file__), "../../dataset", relative_path)
+    joined_path = os.path.join(os.path.dirname(__file__), "../../dataset", relative_path)
+    return os.path.abspath(joined_path)
